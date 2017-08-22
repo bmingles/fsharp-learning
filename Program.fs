8ca0b923f539dea2e03e5949e4e8fa4912d83bd1
Brian Ingles <bingles@athenahealth.com>
2017-08-22 08:19:51 -0400

    p. 13

---
+++ Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module MiniSuave
+open Suave.Http
+open Suave.Console
+open Suave.Successful
+
+[<EntryPoint>]
+let main argv =
+    let request = { Route = ""; Type = Suave.Http.GET }
+    let response = { Content = ""; StatusCode = 200 }
+    let context = { Request = request; Response = response }
+
+    execute context (OK "Hello World!")
+
+    0
---
+++ Suave.fs
@@ -0,0 +1,64 @@
+namespace Suave
+module Http =
+    type RequestType = GET | POST
+    type Request = {
+        Route : string
+        Type : RequestType
+    }
+    type Response = {
+        Content : string
+        StatusCode : int
+    }
+    type Context = {
+        Request : Request
+        Response : Response
+    }
+    type WebPart = Context -> Async<Context option>
+
+module Successful =
+    open Http
+
+    let OK content context =
+        {context with Response = {Content = content; StatusCode = 200}}
+        |> Some
+        |> async.Return
+
+module Console =
+    open Http
+
+    let execute inputContext webpart =
+        async {
+            let! outputContext = webpart inputContext
+            match outputContext with
+            | Some context ->
+                printfn "------------------------"
+                printfn "Code : %d" context.Response.StatusCode
+                printfn "Output : %s" context.Response.Content
+                printfn "------------------------"
+            | None ->
+                printfn "No Output"
+        } |> Async.RunSynchronously
+
+    let parseRequest (input : System.String) =
+        let parts = input.Split([|';'|])
+        let rawType = parts.[0]
+        let route = parts.[1]
+        match rawType with
+        | "GET" -> { Type = GET; Route = route }
+        | "POST" -> { Type = POST; Route = route }
+        | _ -> failwith "invalid request"
+
+    let executeInLoop inputContext webpart =
+        let mutable continueLooping = true
+        while continueLooping do
+            printf "Enter Input Route : "
+            let input = System.Console.ReadLine()
+            try
+                if input = "exit" then
+                    continueLooping <- false
+                else
+                    let context = { inputContext with Request = parseRequest input }
+                    execute context webpart
+                with
+                    | ex ->
+                      printfn "Error : %s" ex.Message
